3d9ff6d7aeb96f583ea1fac0fd0c1d56b754f78a
sarafbrun <sarafuen.93@gmail.com>
2023-02-24 18:43:41 +0100

    iniciada maquetacion html + capturacion elementos necesarios

---
+++ js/view.js
@@ -0,0 +1,4 @@
+const input = document.querySelector('input');
+const addbtn = document.querySelector('.addbtn');
+const ul = document.querySelector('ul');
+const empty = document.querySelector('.empty');
